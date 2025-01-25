983cebf4dd45c23b4c8be3c2a1a709319f6ae4e1
42pupusas <technology@illuminodes.com>
2025-01-24 21:01:42 -0600

    fix build step

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
-	let protos = &["proto/nostr.proto"];
+    #[cfg(feature = "build")]
+    let protos = &["proto/nostr.proto"];
 
-	// server
-	tonic_build::configure()
-		.build_server(true)
-		.build_client(true)
-		.out_dir("src")
-		.compile_protos(protos, &["proto"])
-		.expect("failed to compile arkd server protos");
-
+    #[cfg(feature = "build")]
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir("src")
+        .compile_protos(protos, &["proto"])
+        .expect("failed to compile arkd server protos");
 }
